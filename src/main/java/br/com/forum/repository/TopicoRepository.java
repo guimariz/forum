ea2e534b1d4fbbc180436916a19251773567866d
guimariz <guimariz@gmail.com>
2022-11-16 15:39:47 -0300

    find by curso nome adicionado

--- src/main/java/br/com/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/forum/repository/TopicoRepository.java
@@ -1,11 +1,13 @@
 package br.com.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Integer> {
 
-	
-	
+	List<Topico> findByCursoNome(String nomeCurso);
+
 }

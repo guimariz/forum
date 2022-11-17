99d90fad7e25215ba144955c703030e07707d668
guimariz <guimariz@gmail.com>
2022-11-17 16:29:56 -0300

    paginacao e ordenacao implementadas

--- src/main/java/br/com/forum/controller/repository/TopicoRepository.java
+++ src/main/java/br/com/forum/controller/repository/TopicoRepository.java
@@ -1,13 +1,13 @@
 package br.com.forum.controller.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Integer> {
 
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 
 }

ff42a33aea242ae3767b4250dd02dd56f845aeb5
guimariz <guimariz@gmail.com>
2022-11-17 00:16:09 -0300

    put implementado

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -3,6 +3,7 @@ package br.com.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.forum.controller.dto.DetalhesTopicoDto;
 import br.com.forum.controller.dto.TopicoDto;
+import br.com.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.forum.controller.form.TopicoForm;
 import br.com.forum.controller.repository.CursoRepository;
 import br.com.forum.controller.repository.TopicoRepository;
@@ -56,6 +59,14 @@ public class TopicosController {
 	public DetalhesTopicoDto detalhar(@PathVariable Integer id) {
 		Topico topico = topicoRepository.getReferenceById(id);
 		return new DetalhesTopicoDto(topico);
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Integer id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+		Topico topico = form.atualizar(id, topicoRepository);
+		
+		return ResponseEntity.ok(new TopicoDto(topico));
 	}
 	
 }
